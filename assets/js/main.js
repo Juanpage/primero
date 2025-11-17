9c798e80d484cef430f4df1c0472345fc89a4106
Juanpage <147500160+Juanpage@users.noreply.github.com>
2025-11-17 10:50:06 -0500

    Point FormSubmit submissions to commercial inbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,7 +44,7 @@ const quoteFeedbackEl = document.getElementById("quoteFeedback");
 const qNombreInput = document.getElementById("qNombre");
 const quoteSubmitBtn = quoteForm?.querySelector("button[type='submit'], input[type='submit']");
 const contactSubmitBtn = contactForm?.querySelector("button[type='submit'], input[type='submit']");
-const FORM_SUBMIT_ENDPOINT = "https://formsubmit.co/ajax/webmaster@visitingalapagos.com";
+const FORM_SUBMIT_ENDPOINT = "https://formsubmit.co/ajax/commercial@visitingalapagos.com";
 
 if (quoteSubmitBtn && !quoteSubmitBtn.dataset.originalMarkup) {
   if (quoteSubmitBtn.tagName === "BUTTON") {
